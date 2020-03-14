bf5bb1f73f5f1b202c878fcc2527939600a77238
JW9506 <cf.dota@gmail.com>
2020-03-14 00:34:24 -0400

    init App

---
+++ src/globals.d.ts
@@ -0,0 +1,14 @@
+declare module "*.css" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.scss" {
+  const src: string;
+  export default src;
+}
+
+declare module "Config" {
+  export const PUBLIC_URL: string;
+  export const TITLE: string;
+}
