5eda8503d4580b9c5a6857649842400abe191337
JW9506 <cf.dota@gmail.com>
2020-03-14 06:01:10 -0400

    feat-router-dom--0.1

--- src/js/components/MenuItem/index.tsx
+++ src/js/components/MenuItem/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import clsx from "clsx";
 import "./index.scss";
+import { PUBLIC_URL } from "Config";
+import { useHistory } from "react-router-dom";
 
 interface MenuItemProps {
   title: string;
   imageUrl: string;
+  linkUrl: string;
   size?: "large";
 }
-const MenuItem: React.FC<MenuItemProps> = ({ title, imageUrl, size }) => {
+const MenuItem: React.FC<MenuItemProps> = ({
+  title,
+  imageUrl,
+  size,
+  linkUrl
+}) => {
+  const H = useHistory();
   return (
-    <div className={clsx("menu-item", size)}>
+    <div
+      className={clsx("menu-item", size)}
+      onClick={() => H.push(`${PUBLIC_URL}/${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
